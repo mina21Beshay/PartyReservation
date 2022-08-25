f7f316caf7d4adb7481036a7d448f0b22d873c55
mina21Beshay <79889471+mina21Beshay@users.noreply.github.com>
2022-08-24 22:16:32 -0400

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+using System.Diagnostics;
+
+namespace MvcMovie.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Gallery()
+        {
+            return View();
+        }
+        public IActionResult Rsvp(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Congrats", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+        public IActionResult ListOfGuests()
+        {
+            return View(Repository.Responses.Where(r => r.Parking == true));
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+
+}
---
+++ Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace MvcMovie.Models
+
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your age")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Arrival Time")]
+        public string Time { get; set; }
+
+        [Required(ErrorMessage = "Do you want parking?")]
+        public bool Parking { get; set; }
+    }
+}
